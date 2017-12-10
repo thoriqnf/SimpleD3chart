23e7c0ac2430237d37c6cfd839233b00a78959b5
thoriqnf <thoriqnfaizal@gmail.com>
2017-12-10 15:18:13 +0700

    showing chart

--- web/src/Components/BarChart.js
+++ web/src/Components/BarChart.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import * as d3 from "d3";
+import ReactFauxDOM from "react-faux-dom";
 
 class BarChart extends Component {
   constructor(props) {
@@ -7,8 +9,40 @@ class BarChart extends Component {
       data: props.data
     };
   }
+
+  static defaultProps = {
+    height: 500,
+    width: 500,
+    chartBg: "#f4f4f4",
+    barColor: "steelBlue",
+    barWidth: 40,
+    barOFFset: 5
+  };
+
   render() {
-    return <div>BARCHART</div>;
+    const chart = ReactFauxDOM.createElement("div");
+    d3
+      .select(chart)
+      .append("svg")
+      .attr("width", this.props.width)
+      .attr("height", this.props.height)
+      .style("background", this.props.chartBg)
+      .selectAll("rect")
+      .data(this.state.data)
+      .enter()
+      .append("rect")
+      .style("fill", this.props.barColor)
+      .attr("width", this.props.barWidth)
+      .attr("height", d => {
+        return d;
+      })
+      .attr("x", (d, i) => {
+        return i * (this.props.barWidth + this.props.barOFFset);
+      })
+      .attr("y", d => {
+        return this.props.height - d;
+      });
+    return chart.toReact();
   }
 }
 
